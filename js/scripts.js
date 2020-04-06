eed5b5b428f2720f586072932e47cde8a82dedce
William Pratt <willpratt78@gmail.com>
2020-04-06 14:03:14 -0700

    got output to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// Business Logic
+
+
 function Place(location, landmarks, when, notes, activity) {
   this.location = location;
   this.landmarks = landmarks;
@@ -9,3 +12,19 @@ function Place(location, landmarks, when, notes, activity) {
 Place.prototype.bestTime = function () {
   return this.location + "during the" + this.when
 }
+
+var portland = new Place("Portland", "Rose Garden", "Summer", "be sure to check out Powell's Books", "Hiking");
+
+
+
+
+//UI logic
+$(document).ready(function(){
+  $('#portland').click(function (event) {
+    event.preventDefault();
+    $("#portland").text(portland.location + " is beautiful in the " + portland.when + ". Stop by the " + portland.landmarks + " and  " + portland.notes + " after going " + portland.activity + ".");
+  });
+});
+
+
+
